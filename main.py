71156e6d5bb6164ece6d2432b86d6d0b05594a7d
Dyeadal <dyeadal@gmail.com>
2023-02-03 16:33:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,98 @@
 import sys
-import os
+import os # lists directories, and its files. Also can see modification dates
 import platform #Detects OS platform to run correct copy and hash commands.
 
 # Main function
 if __name__ == "__main__":
-    
-# If no arguments were provided, running the script as is
-    if len(sys.argv) == 1: 
-        print ("\nDyeadal's Incremental Backup\n-------------------------------\n")
-        print("No arguments supplied\nUse --help to print options available\n\n")
 
-    
-# If arguments were provided (meat and potatoes of script)
-    else:
-
-    # System information for OS identification and command execution
-        localPlatform = platform.system()
-        print("Platform: " + localPlatform) #debug
-
-    #creates a deep copy array of the arguments  
-        argList = sys.argv.copy()
-        argList.pop(0) #removes the first (0) argument or item in the list which is the command to run the script itself or location of the script
+# Computer Information | Default and Environmental Variables
+    opsysPlatform = platform.system()
+    toFile = "Not yet chosen"
+    fromFile = "Not yet chosen"
 
 
 
-    #if "--help" is used
-        if "--help" in argList:
-            print ("\nDyeadal's Incremental Backup\n-------------------------------\n")
-            print("\nCommon Usages:\nBack up files missing from a source directory to an existing directory: -i [input_directory] -b [backup_directory]")
+# Main Menu Function
+    def mainMenu():
+        print("\nDyeadal's Incremental Backup\n-------------------------------")
+        print("Computer Information: " + opsysPlatform + "\n")
+        print("Source Directory: " + fromFile + "\t\t\tArchive Directory: " + toFile)
+        print("\n\tC\t-\tChose Directories to compare or create backup")
+        print("\tD\t-\tDisplay Differences")
+        print("\tI\t-\tPerform an Incremental Backup")
+        print("\n\tQ\t-\tQuit")
+        option = input("\n\nType Letter and press [Enter]: ")
 
-            print("\n Arguments to use:")
-            print("-b\tBackup directory location of where your archive is stored.")
-            print("-i\tDirectory to back up to. This is used with the [-b] command.")
-            print()
-
-
-
-        # Store -b value as a variable to easily call on backupDir
-        if "-b" in argList: 
-            backupDir = argList[argList.index("-b")+1]
-            print("Directory to store backup: ", backupDir)
-        
-        # Store -i value as a variable to easily call on input Dir
-        if "-i" in argList: 
-            inputDir = argList[argList.index("-i")+1]
-            print("Directory to backup: ", inputDir)
-
-        # If one argument is used and not the other to notify and quit
-        if "-i" and not "-b" in argList:
-            print("\nError: No value for -b supplied.\nUsing -i requires the usage of -b as well. For further help use '--help'")
-
-        elif "-i" and not "-b" in argList:
-            print("\nError: No value for -i supplied.\nUsing -b requires the usage of -i as well. For further help use '--help'")
+        if option.isalpha():
+            if option == "C" or "c": choseDirs()
+            if option == "D" or "d": print("Displaying Differences")
+            if option == "I" or "i": print("Performing Incremental Backup")
+            if option == "Q" or "q": print("Quitting")
+            else:
+                print("Not a registered option, returning to screen")
+                mainMenu()
         else:
-            if "-b" and "-i" in argList:
-        #Confirm both inputs for directories are actual paths
-                if os.path.exists(backupDir and inputDir):
-                    print("Paths are valid")
+            print("Not a valid input, returning to screen")
+            mainMenu()
 
-                    backupArr = []
-                    inputArr = []
-                    removedArr = []
-                    backupHash = []
-                    inputHash = []
+####################### Chose Directories ############################
+# Functions to begin the directory allotment
+# Splitting into multiple functions allows us to repeat a selection process for each individal directory path if it is not a valid input
+    def choseDirs():
+        print("\n\nDirectories for Comparing or Incrementally Backing Up\n")
+        choseSource()
+        choseArchive()
+        print("\n Current/Production Storage Directory: " + fromFile + "\n Directory to Archive to or Compare: " + toFile)
+        mainMenu()
+    # Function to chose archive directory
+    def choseArchive():
+        source = input("Directory to copy from, or compare (usually storage device that needs files backed up): ")
+        if validDir(source):
+            print("Valid: " + source)
+            global fromFile
+            fromFile = source
+        else:
+            print("Invalid Directory/File\n")
+            choseArchive()
+    # Function to chose source directory
+    def choseSource():
+        archive = input("Directory to back up to, or compare to (an archive of existing files): ")
+        if validDir(archive):
+            print("Valid: " + archive)
+            global toFile
+            toFile = archive
+        else:
+            print("Invalid Directory/File\n")
+            choseSource()
+    # Function to validate file path exists
+    def validDir(directorypath):
+        if os.path.exists(directorypath):
+            return True;
+        else: return False;
 
-                #TO DO Loop function to store paths of files and their MD5 hashsums
-                    #inputDir list files
-                    inputArr.append(os.listdir(inputDir))
+####################### Integrity Check ############################
+# Function to show the differences between the two chosen directories
+    def displayDifferences():
+        mainMenu()
+
+
+###################### Termination Options ###############################
+    # Function to return to menu or quit
+    def returnToMenu():
+        option = input("\n\n\nReturn to Menu [Enter] or [Q]uit")
+        if option == "Q" or "q": terminate()
+        else: mainMenu()
+
+    #Function to quit program
+    def terminate():
+        print("\nGood bye :)")
+
+
+
+
+#TO DO Loop function to store paths of files and their MD5 hashsums
+    #inputDir list files
+
 
                 #TO DO while loop to recursively list and store directory values
                     #check if any are directories in array
@@ -74,9 +100,8 @@ if __name__ == "__main__":
                     #loop till no directories left undocumented
                                         
                     #print output of array to see append worked
-                    print(inputArr)
 
-                #TO DO Research either os or subprocess is better for running MD5 hashsum command in Pythong
+                #TO DO Research either os or subprocess is better for running MD5 hashsum command in Python
                 #EITHER:
                     #os.popen(Get-FileHash <filepath> -Algorithm MD5).read()
                 #OR:
@@ -91,23 +116,10 @@ if __name__ == "__main__":
                 #TO DO: Save changes to an XML file prior to initating backup, as a forensic log
 
 
-                    print("\nIncremental Copy From:", inputDir, "\t\tTo:", backupDir,"?")
-        #Confirm prompt to ensure the user is aware of the changes
-                    confirmPrompt = input("(Y/N): ")
-                    if confirmPrompt == str("y" or "Y"):
-                        print("Copying")    
-                    
-
                 # TO DO Copy file functions of missing or modified files
                     #
 
                 # TO DO Remove files that were removed in input directory from back up archive if they were removed
-                    #
 
-                    elif confirmPrompt == str("n" or "N"):
-                        print("\nCanceling backup")
-                    else:
-                        print("\nError: Invalid input, use --help for assitance")
-
-            # if path validation fails print error
-                else: print ("\nError: Paths used can not be found")
+    #Starts program
+    mainMenu()
